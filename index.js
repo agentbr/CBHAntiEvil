991fd376aac89cc74b70cd35edf2772ae94418b6
KK7IXS <99224020+agentbr@users.noreply.github.com>
2022-11-25 20:19:47 -0800

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,24 @@ client.once('ready', () => {
 // Login to Discord with your client's token
 client.login(config.token);
 
+const { Client, Interaction } = require("discord.js");
+
+module.exports = {
+  name: "ping",
+  description: "Get Ping of Bot",
+  type: "CHAT_INPUT",
+  /**
+   *
+   * @param {Client} client
+   * @param {Interaction} interaction
+   */
+  execute: async (client, interaction) => {
+    try {
+      const mesg = await interaction.reply({ content: "Pong!", fetchReply: true });
+
+      await interaction.editReply({ content: `Pong!\nBot Latency: \`${mesg.createdTimestamp - interaction.createdTimestamp}ms\`, Websocket Latency: \`${client.ws.ping}ms\`` });
+    } catch (err) {
+      console.log("Something Went Wrong => ", err);
+    }
+  },
+};
